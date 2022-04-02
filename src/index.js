f0a602e4fd7b968b51030e9173746f9a725ae01f
Oskar Risberg <oskar.risberg@stendahls.se>
2022-04-02 16:32:06 +0200

    Cleanup from CRA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import 'typeface-roboto';
 import {
     BrowserRouter as Router,
@@ -18,16 +19,18 @@ const queryClient = new QueryClient();
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 
 root.render((
-    <QueryClientProvider
-        client = {queryClient}
-    >
-        <Router>
-            <App />
-        </Router>
-    </QueryClientProvider>
+    <React.StrictMode>
+        <QueryClientProvider
+            client = {queryClient}
+        >
+            <Router>
+                <App />
+            </Router>
+        </QueryClientProvider>
+    </React.StrictMode>
 ));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
