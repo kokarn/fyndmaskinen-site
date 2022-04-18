5cf5138c52928d5d074b6c62dd342c5fc491f664
Oskar Risberg <oskar.risberg@stendahls.se>
2022-04-18 12:05:34 +0200

    Add service-worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 import 'typeface-roboto';
 import {
     BrowserRouter as Router,
@@ -44,5 +45,10 @@ root.render((
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
