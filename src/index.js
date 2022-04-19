e12ccde0ddac69aafcc4c89b4c2b5e91f5174355
Oskar Risberg <oskar.risberg@stendahls.se>
2022-04-19 09:23:37 +0200

    switch how we load fonts

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import 'typeface-roboto';
 import {
     BrowserRouter as Router,
 } from 'react-router-dom';
