dff51a821cadb3078c2271cc874f7dac1cab1a85
Oskar Risberg <oskar.risberg@stendahls.se>
2022-11-14 15:26:47 +0100

    remove property not used

--- src/components/search-table/index.js
+++ src/components/search-table/index.js
@@ -99,7 +99,6 @@ const SearchTable = ({
                             <CardMedia
                                 alt = {tile.title}
                                 component = 'img'
-                                height = {'100%'}
                                 image = {`https://wsrv.nl/?url=${ tile.imageUrl }&w=400&h=400&fit=contain&trim=10&errorredirect=${ errorImage }&output=webp`}
                             />
                             <CardContent>
